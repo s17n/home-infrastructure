cf3527f51c16e29ef3b0d69e9f4532cce8030ae5
s17n <wusterhausen@googlemail.com>
2023-03-15 10:17:34 +0100

    added influxdb2

---
+++ influxdb2/telegraf/createTibberDataDirs.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+rootDir=$HOME/data/tibber
+
+mkdir -p $rootDir/data-consumption-error
+mkdir -p $rootDir/data-consumption-finished
+mkdir -p $rootDir/data-input
+mkdir -p $rootDir/data-priceinfo-error
+mkdir -p $rootDir/data-priceinfo-finished
+chmod 777 $rootDir/*
