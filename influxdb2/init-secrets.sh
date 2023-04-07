d0b0b0f5ef444a159fa1a2a6493ff44470022d6c
s17n <wusterhausen@googlemail.com>
2023-04-07 06:34:29 +0200

    secrets updated

--- influxdb2/init-secrets.sh
+++ influxdb2/init-secrets.sh
@@ -3,3 +3,5 @@
 composeDir="$(dirname $0)"
 
 op inject -f -i $composeDir/.secrets.template.env -o $composeDir/.secrets.env
+
+op inject -f -i $composeDir/config/influx-configs.template -o $composeDir/config/influx-configs
